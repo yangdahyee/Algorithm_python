e06d01a1585342b62800c83f4846ee214e63b0f3
양다혜 <dahhhy17@gmail.com>
2025-08-22 07:57:05 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,17 @@
+grade_dict = { "A+" : 4.5, "A0" : 4.0, "B+" : 3.5, "B0" : 3.0, "C+" : 2.5, "C0" : 2.0, "D+" : 1.5, "D0" : 1.0, "F" : 0.0
+}
+
+total_score = 0.0  
+total_credit = 0.0 
+
+for _ in range(20):
+    subject, credit, grade = input().split()
+    credit = float(credit)
+    
+    if grade == "P":   
+        continue
+    
+    total_score += credit * grade_dict[grade]
+    total_credit += credit
+
+print(total_score / total_credit)
