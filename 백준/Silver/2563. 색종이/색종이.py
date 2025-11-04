10b1ca31e70f495a4661b6db915b3209d0e09e83
양다혜 <dahhhy17@gmail.com>
2025-11-05 01:09:23 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+paper = [[0]*100 for _ in range(100)]  
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            paper[i][j] = 1
+
+ans = 0
+for i in range(100):
+    for j in range(100):
+        ans += paper[i][j]
+
+print(ans)
