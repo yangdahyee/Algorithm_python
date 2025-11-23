2a74e074aafa9d0e7ad57479787598ef77487e7d
양다혜 <dahhhy17@gmail.com>
2025-11-24 02:00:42 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 108 ms, Memory: 38948 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+K = int(input())
+
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        dp[i][j] = arr[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
+
+for _ in range(K):
+    i, j, x, y = map(int, input().split())
+    ans = dp[x][y] - dp[i-1][y] - dp[x][j-1] + dp[i-1][j-1]
+    print(ans)
