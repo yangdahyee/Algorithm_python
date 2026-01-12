eb1fb495a3b5742caf9b9441164abe4741c7c9ee
양다혜 <dahhhy17@gmail.com>
2026-01-12 20:40:31 +0900

    [Gold IV] Title: 오큰수, Time: 1020 ms, Memory: 136860 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = [-1] * N
+stack = []
+
+
+for i in range(N):
+    while stack and A[stack[-1]] < A[i]:
+        ans[stack.pop()] = A[i]
+    stack.append(i)
+
+print(*ans)
