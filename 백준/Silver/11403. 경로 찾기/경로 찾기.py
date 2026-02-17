cfd2d2866762181d607789e3055516e65b0426a3
양다혜 <dahhhy17@gmail.com>
2026-02-17 23:18:58 +0900

    [Silver I] Title: 경로 찾기, Time: 216 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+for row in graph:
+    print(*row)
