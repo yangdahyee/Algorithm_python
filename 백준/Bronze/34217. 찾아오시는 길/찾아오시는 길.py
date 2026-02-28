9a3874e56af0e8819bc6aceca79dcfe144fda7a5
양다혜 <dahhhy17@gmail.com>
2026-02-28 22:59:25 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+C, D = map(int, input().split())
+
+hanyang = A + C
+yongdap = B + D
+
+if hanyang < yongdap:
+    print("Hanyang Univ.")
+elif hanyang > yongdap:
+    print("Yongdap")
+else:
+    print("Either")
